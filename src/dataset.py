5e60ce4364e148b8644bb1e1265d84cfedf4f41f
Hugo Johnson <hugojohnson271@gmail.com>
2024-11-19 17:03:07 +0100

    fix: integrating Julia's code checkpoint

--- src/dataset.py
+++ src/dataset.py
@@ -32,34 +32,31 @@ def load_dataframes():
     print("Generated and saved to pickle")
     return dataframes
 
-def filter_df():
+def filter_df(input_features, output_features):
     # Læs og kombiner alle datasæt i én DataFrame
     dataframes = load_dataframes()
     combined_df = pd.concat(dataframes, ignore_index=True)
-
-    # print("Kombineret datasæts form:", combined_df.shape)
-
-    # Udvælg de input features, du ønsker at anvende
-    input_features = ['beta1', 'beta2', 'beta3', 'Theta', 'omega_r', 'Vwx']
-
-    # Udvælg de output features, du ønsker at forudsige
-    output_features = ['Mz1', 'Mz2', 'Mz3']
 
     # Filtrer data til kun at indeholde de ønskede kolonner
     filtered_df = combined_df[input_features + output_features]
     return filtered_df
 
+def normalize_data():
+    # Udvælg de input features, du ønsker at anvende
+    input_features = ['beta1', 'beta2', 'beta3', 'Theta', 'omega_r', 'Vwx']
+    # Udvælg de output features, du ønsker at forudsige
+    output_features = ['Mz1', 'Mz2', 'Mz3']
+
+    filtered_df = filter_df(input_features, output_features)
+    scaler_X = MinMaxScaler()
+    scaler_y = MinMaxScaler()
+
+
+    X = scaler_X.fit_transform(filtered_df[input_features])
+    y = scaler_y.fit_transform(filtered_df[output_features])
+    return X, y
+
 def load_and_split_data():
-    # pickle_path = os.path.join('data', 'processed', 'split_data.pkl')
-    # if os.path.exists(pickle_path):
-    #     # Load the dictionary from the file
-    #     with open(pickle_path, "rb") as f:
-    #         loaded_dict = pickle.load(f)  
-    #     print("Loaded from pickle")
-    #     return loaded_dict
-    filtered_df = filter_df()
-
-
     # # =====
     # # Normaliser dataene
     # scaler = MinMaxScaler()
@@ -69,14 +66,7 @@ def load_and_split_data():
     # X = combined_df_scaled[input_features]
     # y = combined_df_scaled[output_features]
     # # =====
-
-    # Normaliser dataene
-    scaler_X = MinMaxScaler()
-    scaler_y = MinMaxScaler()
-
-
-    X = scaler_X.fit_transform(filtered_df[input_features])
-    y = scaler_y.fit_transform(filtered_df[output_features])
+    X, Y = load_and_split_data()
 
     # Split data i træning og test
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
